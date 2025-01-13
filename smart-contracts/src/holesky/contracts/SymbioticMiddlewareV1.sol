368aff41f08042f5988af58b99d4a038625793b9
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-01-13 16:29:59 +0100

    feat(contracts): wip symbiotic, fixes

--- smart-contracts/src/holesky/contracts/SymbioticMiddlewareV1.sol
+++ smart-contracts/src/holesky/contracts/SymbioticMiddlewareV1.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 import {Operators} from "@symbiotic/middleware-sdk/extensions/operators/Operators.sol";
 import {SharedVaults} from "@symbiotic/middleware-sdk/extensions/SharedVaults.sol";
 import {KeyManagerAddress} from "@symbiotic/middleware-sdk/extensions/managers/keys/KeyManagerAddress.sol";
+import {EqualStakePower} from "@symbiotic/middleware-sdk/extensions/managers/stake-powers/EqualStakePower.sol";
+import {EpochCapture} from "@symbiotic/middleware-sdk/extensions/managers/capture-timestamps/EpochCapture.sol";
 import {ECDSASig} from "@symbiotic/middleware-sdk/extensions/managers/sigs/ECDSASig.sol";
+import {OwnableAccessManager} from "@symbiotic/middleware-sdk/extensions/managers/access/OwnableAccessManager.sol";
 
 import {Subnetwork} from "@symbiotic/core/contracts/libraries/Subnetwork.sol";
+import {INetworkRegistry} from "@symbiotic/core/interfaces/INetworkRegistry.sol";
 
 /**
  * @title SymbioticMiddlewareV1
@@ -19,8 +22,17 @@ import {Subnetwork} from "@symbiotic/core/contracts/libraries/Subnetwork.sol";
  *      - Operators: Provides operator management (keys, vaults, registration)
  *      - KeyManagerAddress: Manages storage and validation of operator keys using address values
  *      - ECDSASig: Verify ECDSA keys against operator addresses
+ *      - EqualStakePower: Equal stake to power for all registered vaults
  */
-contract SymbioticMiddlewareV1 is Operators, KeyManagerAddress, ECDSASig, OwnableUpgradeable, UUPSUpgradeable {
+contract SymbioticMiddlewareV1 is
+    Operators,
+    KeyManagerAddress,
+    ECDSASig,
+    EqualStakePower,
+    EpochCapture,
+    OwnableAccessManager,
+    UUPSUpgradeable
+{
     using Subnetwork for address;
 
     /**
@@ -33,11 +45,17 @@ contract SymbioticMiddlewareV1 is Operators, KeyManagerAddress, ECDSASig, Ownabl
      */
     uint256[50] private __gap;
 
+    modifier onlyOwner() {
+        _checkAccess();
+        _;
+    }
+
     /**
-     * @notice Constructor for initializing the SelfRegisterMiddleware contract
+     * @notice Constructor for initializing the SymbioticMiddlewareV1 contract
      * @param owner The address of the owner
      * @param network The address of the network
-     * @param slashingWindow The duration of the slashing window
+     * @param networkRegistry The address of the network registry
+     * @param slashingWindow The duration of the slashing window (unused in V1)
      * @param vaultRegistry The address of the vault registry
      * @param operatorRegistry The address of the operator registry
      * @param operatorNetOptin The address of the operator network opt-in service
@@ -46,18 +64,21 @@ contract SymbioticMiddlewareV1 is Operators, KeyManagerAddress, ECDSASig, Ownabl
     function initialize(
         address owner,
         address network,
+        address networkRegistry,
         uint48 slashingWindow,
         address vaultRegistry,
         address operatorRegistry,
         address operatorNetOptin,
         address reader
-    ) internal initializer {
-        // NOTE: the reader is just a separate contract that can be deployed with
-        //         address readHelper = address(new BaseMiddlewareReader());
+    ) public initializer {
+        // Register the network
         INetworkRegistry(networkRegistry).registerNetwork();
 
-        __Ownable_init(owner);
+        // Initialize middleware
         __BaseMiddleware_init(network, slashingWindow, vaultRegistry, operatorRegistry, operatorNetOptin, reader);
+
+        // Initialize owner access
+        __OwnableAccessManager_init(owner);
     }
 
     function _authorizeUpgrade(
