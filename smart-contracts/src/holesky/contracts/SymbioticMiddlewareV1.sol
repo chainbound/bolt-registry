21f013d0482c78d8e3003f851b1507ac98e75d5e
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-01-13 13:47:21 +0100

    feat(contracts): import middleware-sdk

--- smart-contracts/src/holesky/contracts/SymbioticMiddlewareV1.sol
+++ smart-contracts/src/holesky/contracts/SymbioticMiddlewareV1.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-contract SymbioticMiddlewareV1 {}
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+import {BaseMiddleware} from "@symbiotic/middleware-sdk/middleware/BaseMiddleware.sol";
+import {OwnableAccessManager} from "@symbiotic/middleware-sdk/extensions/managers/access/OwnableAccessManager.sol";
+
+// import {Subnetwork} from "@symbiotic/contracts/libraries/Subnetwork.sol";
+
+contract SymbioticMiddlewareV1 is BaseMiddleware, OwnableUpgradeable, UUPSUpgradeable {
+    using Subnetwork for address;
+}
