04d13f225f871c414b4e29af1d1c90700884cf67
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-01-13 15:58:10 +0100

    feat(contracts): scaffold deployment script for clarity"

--- smart-contracts/script/holesky/Deploy.s.sol
+++ smart-contracts/script/holesky/Deploy.s.sol
@@ -4,11 +4,28 @@ pragma solidity 0.8.25;
 import {Script, console} from "forge-std/Script.sol";
 
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {Upgrades, Options} from "@openzeppelin-foundry-upgrades/src/Upgrades.sol";
+import {Upgrades, Options} from "@openzeppelin/foundry-upgrades/src/Upgrades.sol";
+
+import {BaseMiddlewareReader} from "@symbiotic/middleware-sdk/middleware/BaseMiddlewareReader.sol";
+
+import {SymbioticMiddlewareV1} from "../../src/holesky/contracts/SymbioticMiddlewareV1.sol";
 
 contract DeployRegistry is Script {
     function main() public {
-        address registry = deploy("OperatorsRegistryV1");
-        console.log("Registry deployed at: ", registry);
+        address admin = msg.sender;
+        address readerHelper = address(new BaseMiddlewareReader());
+
+        // TODO: Fix safe deploy, currently failing with `ASTDereferencerError` from openzeppelin
+        Options memory opts;
+        opts.unsafeSkipAllChecks = true;
+
+        bytes memory initParams = abi.encodeCall(
+            SymbioticMiddlewareV1.initialize,
+            (
+                admin,
+                // TODO
+                readerHelper,
+            )
+        );
     }
 }
