606f515b464c9b516e284c8b7972ebaca47da0de
nicolas <48695862+merklefruit@users.noreply.github.com>
2025-01-20 17:09:28 +0100

    feat: working collateral deposit test

--- smart-contracts/test/holesky/BoltEigenLayerMiddlewareV1.t.sol
+++ smart-contracts/test/holesky/BoltEigenLayerMiddlewareV1.t.sol
@@ -6,6 +6,7 @@ import {IERC20} from "@openzeppelin/contracts-eigenlayer/token/ERC20/IERC20.sol"
 import {
     IAllocationManager, IAllocationManagerTypes
 } from "@eigenlayer/src/contracts/interfaces/IAllocationManager.sol";
+import {AllocationManagerStorage} from "@eigenlayer/src/contracts/core/AllocationManagerStorage.sol";
 import {IDelegationManager} from "@eigenlayer/src/contracts/interfaces/IDelegationManager.sol";
 import {IStrategyManager} from "@eigenlayer/src/contracts/interfaces/IStrategyManager.sol";
 import {IStrategy} from "@eigenlayer/src/contracts/interfaces/IStrategy.sol";
@@ -15,6 +16,7 @@ import {OperatorSet} from "@eigenlayer/src/contracts/libraries/OperatorSetLib.so
 import {OperatorsRegistryV1} from "../../src/holesky/contracts/OperatorsRegistryV1.sol";
 import {IOperatorsRegistryV1} from "../../src/holesky/interfaces/IOperatorsRegistryV1.sol";
 import {BoltEigenLayerMiddlewareV1} from "../../src/holesky/contracts/BoltEigenLayerMiddlewareV1.sol";
+import {IWETH} from "./util/IWETH.sol";
 
 contract BoltEigenLayerMiddlewareV1Test is Test {
     OperatorsRegistryV1 registry;
@@ -28,11 +30,14 @@ contract BoltEigenLayerMiddlewareV1Test is Test {
     IDelegationManager holeskyDelegationManager = IDelegationManager(0xA44151489861Fe9e3055d95adC98FbD462B948e7);
     IStrategyManager holeskyStrategyManager = IStrategyManager(0xdfB5f6CE42aAA7830E94ECFCcAd411beF4d4D5b6);
 
-    // StEth Proxy: 0x3F1c547b21f65e10480dE3ad8E19fAAC46C95034
-    IERC20 holeskyStEth = IERC20(0x59034815464d18134A55EED3702b535D8A32c52b);
+    IERC20 holeskyStEth = IERC20(0x3F1c547b21f65e10480dE3ad8E19fAAC46C95034);
     IStrategy holeskyStEthStrategy = IStrategy(0x7D704507b76571a51d9caE8AdDAbBFd0ba0e63d3);
 
-    // Note: operator signature check can be skipped for testing purposes. This is a no-op signature.
+    IERC20 holeskyWeth = IERC20(0x94373a4919B3240D86eA41593D5eBa789FEF3848);
+    IStrategy holeskyWethStrategy = IStrategy(0x80528D6e9A2BAbFc766965E0E26d5aB08D9CFaF9);
+
+    // Note: operators can gate staker delegations behind a signature check. when this is disabled,
+    // a signature is still required but it can be empty. This is a no-op signature.
     ISignatureUtils.SignatureWithExpiry NoOpSignature = ISignatureUtils.SignatureWithExpiry(bytes(""), 0);
 
     function setUp() public {
@@ -41,9 +46,6 @@ contract BoltEigenLayerMiddlewareV1Test is Test {
         admin = makeAddr("admin");
         operator = makeAddr("operator");
         staker = makeAddr("staker");
-
-        vm.deal(admin, 1000 ether);
-        vm.deal(staker, 1000 ether);
 
         vm.startPrank(admin);
 
@@ -57,16 +59,18 @@ contract BoltEigenLayerMiddlewareV1Test is Test {
             admin, holeskyAllocationManager, holeskyDelegationManager, holeskyStrategyManager, registry
         );
 
-        // 1. Whitelist the strategy in the middleware
+        // 1. Whitelist the strategies in the middleware
         middleware.addStrategyToWhitelist(address(holeskyStEthStrategy));
+        middleware.addStrategyToWhitelist(address(holeskyWethStrategy));
 
-        IStrategy[] memory strategies = new IStrategy[](1);
+        IStrategy[] memory strategies = new IStrategy[](2);
         strategies[0] = holeskyStEthStrategy;
+        strategies[1] = holeskyWethStrategy;
 
         IAllocationManagerTypes.CreateSetParams[] memory createParams = new IAllocationManagerTypes.CreateSetParams[](1);
         createParams[0] = IAllocationManagerTypes.CreateSetParams({operatorSetId: 0, strategies: strategies});
 
-        // 2. Create an OperatorSet in the middleware (with the stEth strategy)
+        // 2. Create an OperatorSet in the middleware to handle all strategies
         middleware.createOperatorSets(createParams);
 
         // 3. Add the middleware address to the registry
@@ -81,8 +85,11 @@ contract BoltEigenLayerMiddlewareV1Test is Test {
     /// Helper function to register an operator
     function _registerOperator(address signer, string memory rpcEndpoint) public {
         // 1. Register the new EL operator in DelegationManager
+        uint32 allocationDelay = 1;
+        address delegationApprover = address(0x0); // this is optional, skip it
+        string memory uri = "some-meetadata.uri.com";
         vm.prank(operator);
-        holeskyDelegationManager.registerAsOperator(address(0x0), 0, "my-metadata-uri.com");
+        holeskyDelegationManager.registerAsOperator(delegationApprover, allocationDelay, uri);
 
         // 2. Register the operator in the bolt AVS
         vm.prank(operator);
@@ -94,29 +101,33 @@ contract BoltEigenLayerMiddlewareV1Test is Test {
                 data: bytes(rpcEndpoint)
             })
         );
+
+        // 3. make sure to enable allocations by skipping the builtin delay blocks
+        uint32 delay = AllocationManagerStorage(address(holeskyAllocationManager)).ALLOCATION_CONFIGURATION_DELAY();
+        vm.roll(block.number + delay + 1);
     }
 
-    function testRegisterOperator() public {
-        string memory rpcEndpoint = "http://localhost:8545";
-
-        _registerOperator(operator, rpcEndpoint);
+    function testRegister() public {
+        _registerOperator(operator, "http://stopjava.com");
     }
 
-    function testDepositCollateral() public {
-        string memory rpcEndpoint = "http://localhost:8545";
-
-        // 0. Mint stEth to the staker
-        // TODO
-
-        // 1. Deposit stEth collateral to the operator
+    function testRegisterAndDepositCollateral() public {
+        // 0. Mint weth to the staker
+        assertEq(address(holeskyWethStrategy.underlyingToken()), address(holeskyWeth));
+        vm.deal(staker, 100 ether);
         vm.prank(staker);
-        holeskyStEth.approve(address(holeskyStrategyManager), 100 ether);
+        IWETH(address(holeskyWeth)).deposit{value: 100 ether}();
+        assertEq(holeskyWeth.balanceOf(staker), 100 ether);
+
+        // 1. Deposit weth collateral to the operator
         vm.prank(staker);
-        uint256 shares = holeskyStrategyManager.depositIntoStrategy(holeskyStEthStrategy, holeskyStEth, 100 ether);
-        assertEq(holeskyStEthStrategy.sharesToUnderlyingView(shares), 100 ether);
+        holeskyWeth.approve(address(holeskyStrategyManager), 100 ether);
+        vm.prank(staker);
+        uint256 shares = holeskyStrategyManager.depositIntoStrategy(holeskyWethStrategy, holeskyWeth, 100 ether);
+        assertEq(holeskyWethStrategy.sharesToUnderlyingView(shares), 100 ether);
 
         // 2. Register the operator in both EL and the bolt AVS
-        _registerOperator(operator, rpcEndpoint);
+        _registerOperator(operator, "http://stopjava.com");
 
         // 3. Delegate funds from the staker to the operator
         vm.prank(staker);
@@ -126,7 +137,7 @@ contract BoltEigenLayerMiddlewareV1Test is Test {
         // 4. Update operator allocation for the bolt avs
 
         IStrategy[] memory strategies = new IStrategy[](1);
-        strategies[0] = holeskyStEthStrategy;
+        strategies[0] = holeskyWethStrategy;
 
         uint64[] memory newMagnitudes = new uint64[](1);
         newMagnitudes[0] = 100;
@@ -143,8 +154,15 @@ contract BoltEigenLayerMiddlewareV1Test is Test {
 
         // 5. try to read the operator's collateral directly on the avs
         (address[] memory collaterals, uint256[] memory amounts) = middleware.getOperatorCollaterals(operator);
-        assertEq(collaterals.length, 1);
-        assertEq(amounts.length, 1);
+        assertEq(collaterals.length, 2);
+        assertEq(amounts.length, 2);
+        
+        // stEth should have 0 balance
         assertEq(collaterals[0], address(holeskyStEth));
+        assertEq(amounts[0], 0);
+
+        // weth should have 100e18 balance
+        assertEq(collaterals[1], address(holeskyWeth));
+        assertEq(amounts[1], 100 ether);
     }
 }
